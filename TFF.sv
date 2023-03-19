47b002a5da37f88bd3a01df534c2721498f737ca
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-03-19 15:09:44 +0530

    Update TFF.sv

--- TFF.sv
+++ TFF.sv
@@ -22,16 +22,17 @@
 // Add a 2:1 mux to the DUV
 interface T_TB (clk);
     input clk;
-    logic T, rst;
+    logic [1:0]T;
+    logic sel, rst;
     logic Q;
 
-    modport TFF_ports(input T, rst, clk, output Q);
+    modport TFF_ports(input T, sel, rst, clk, output Q);
 
 
     parameter tsetup = 2, thold = 3;
     clocking TBForce @(posedge clk);
         default input #(tsetup) output #(thold);
-        output T, rst;
+        output T, sel, rst;
         input Q;
     endclocking: TBForce
 
@@ -44,12 +45,16 @@ interface T_TB (clk);
     endtask: TestRst
 
     task TestValues;
-        input T;
+        input [1:0]T;
+        input sel;
+        bit t;
         static reg PrevQ = TBForce.Q;
         TBForce.T <= T;
-        repeat (2) @(TBForce);
-        $write("%1t: T = %b --> PrevQ = %b, Q = %b", $time, T, PrevQ, TBForce.Q); 
-        case (T)
+        TBForce.sel <= sel;
+        assign t = sel?(T[1]):(T[0]);
+        repeat (2) @(posedge TBForce);
+        $write("%t: T = %b, sel = %d --> PrevQ = %b, Q = %b", $time, T, sel, PrevQ, TBForce.Q);
+        case (t)
             1'd0: Checkif(TBForce.Q != PrevQ);
             1'd1: Checkif(TBForce.Q == PrevQ);
             default: $display("No Values forced");
@@ -68,12 +73,15 @@ interface T_TB (clk);
 endinterface: T_TB
 
 module TFF(T_TB.TFF_ports T_IF);
+    logic t;
+    always_comb
+        t = T_IF.sel?(T_IF.T[1]):(T_IF.T[0]);
     always_ff @(posedge T_IF.clk)
     begin
         if (T_IF.rst)
             T_IF.Q <= 0;
         else
-            case(T_IF.T)
+            case(t)
                 1'd0: T_IF.Q <= T_IF.Q;
                 1'd1: T_IF.Q <= ~T_IF.Q;
             endcase
@@ -85,8 +93,9 @@ module TTestCases(T_TB.TB_ports TB_IF);
     begin
         @(TB_IF.TBForce);
         TB_IF.TestRst;
-        for (int T=0; T<2; T++)
-            TB_IF.TestValues(T);
+        for (int T=0; T<4; T++)
+            for(int sel=0; sel<2; sel++)
+                TB_IF.TestValues(T, sel);
     end
 endmodule: TTestCases
 
