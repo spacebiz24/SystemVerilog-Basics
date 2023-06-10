3f65062e34da820165f82bdccf73718b2779f5c6
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-06-10 06:37:51 +0530

    Create Design.sv

---
+++ DFF-Complete-Verification-Environment/Design.sv
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module DFF(DUT_interface.DUT DFF_interface);
+    logic D;
+    
+    assign D = DFF_interface.sel?(DFF_interface.D[1]):(DFF_interface.D[0]);
+    
+    always_ff @(posedge DFF_interface.clk)
+    begin
+        if(DFF_interface.rst)
+            DFF_interface.Q <= 'b0;
+        else
+            DFF_interface.Q <= D;
+    end
+endmodule
