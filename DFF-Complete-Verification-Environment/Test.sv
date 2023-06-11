7df124b784f416ca662b383eefa15b4f079bb315
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-06-11 11:13:37 +0530

    Update Test.sv

--- DFF-Complete-Verification-Environment/Test.sv
+++ DFF-Complete-Verification-Environment/Test.sv
@@ -1,11 +1,9 @@
 `include "environment.sv"
 
 program test(DUT_interface vintrf);
-    environment env;
-    
     initial
     begin
-        env = new(vintrf);
+        environment env = new(vintrf);
         env.gen.total_packets = 10;
         env.run;
     end
